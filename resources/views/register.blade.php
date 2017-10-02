c90b57248acdd4ed2fbaf38c102cf67ed46731ce
pupFet <imronjay05@gmail.com>
2017-10-02 14:05:26 +0800

    Added jquery files and fixed passwords that didnt match

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,21 +2,21 @@
 <head>
 <title>User Registration</title>
 
- <link rel="stylesheet" type="text/css" a href="{{ asset('/css/register.css') }}">
-<!--  // <script type="text/javascript" src="/var/www/html/Larv/public/js/passerr.js"></script> -->
-<script src="{{ asset('/js/passerr.js') }}"></script>
+<link rel="stylesheet" type="text/css" a href="{{ asset('/css/register.css') }}">
+<script src="{{ asset('/js/jquery.min.js') }}"></script>
+<script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> -->
 </head>
 <body>
-
-<h2 align="center">Sign Up Here!</h2>
-
-<button onClick="document.getElementById('id01').style.display='block'" style="width:auto;">
+<h2 align="center">Blog Site</h2>
+<button id="btnStart" onClick="document.getElementById('id01').style.display='block'" style="width:auto;">
 <div align="center">Sign Up</div>
 </button>
-
+<script src="{{ asset('/js/passerr.js') }}"></script>
 <div id="id01" class="modal" onclick="closeWindow()">
   <span onClick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">×</span>
-  <form id="reg-form" class="modal-content animate" method="post" action="/create" >
+  <form id="reg-form" class="modal-content animate" method="post" action="/create" onsubmit"return checkPass()">
     <div class="container">
         <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
       <label><b>Last Name</b></label>
@@ -24,14 +24,14 @@
       <label><b>First Name</b></label>
         <input type="text" placeholder="Enter First Name" name="firstname" required>
       <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="username" required>
+        <input type="text" placeholder="Enter Username" name="username" required id="username">
       <label><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="password" id="pass" required>
       <label><b>Repeat Password</b></label>
        <input type="password" placeholder="Repeat Password" name="psw-repeat" id="rep_pass" required>
       <div class="clearfix">
         <button type="button" onClick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-        <button class="signupbtn" onclick="checkPass()">Sign Up</button>
+        <button id="sub-form" class="signupbtn" onclick="return checkPass()">Sign Up</button>
       </div>
     </div>
   </form>
